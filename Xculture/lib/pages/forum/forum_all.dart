a8aad55b45b28339e9edb1ee02a55b2e2b3d18e9
FennecFoxy11 <yosita.yomook@mail.kmutt.ac.th>
2022-01-26 21:15:44 +0700

    Fix see all

--- Xculture/lib/pages/forum/forum_all.dart
+++ Xculture/lib/pages/forum/forum_all.dart
@@ -143,8 +143,8 @@ class _ForumAllPageState extends State<ForumAllPage> {
                       // padding: const EdgeInsets.all(20.0),
                       child: 
                         Container(
-                          margin: const EdgeInsets.all(10),
-                          height: 80,
+                          margin: const EdgeInsets.all(20.0),
+                          height: 100,
                           child: Row(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
@@ -171,62 +171,54 @@ class _ForumAllPageState extends State<ForumAllPage> {
                                   child: Column(
                                     crossAxisAlignment: CrossAxisAlignment.start,
                                     children: [
-                                      Expanded(
-                                        child: Column(
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: [
-                                            Text(snapshot.data![index].title,
-                                              maxLines: 2,
-                                              overflow: TextOverflow.ellipsis,
-                                              style: const TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                              ),
+                                      Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text(snapshot.data![index].title,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
                                             ),
-                                            const Padding(padding: EdgeInsets.only(bottom: 2.0)),
-                                            Text(snapshot.data![index].subtitle,
-                                              maxLines: 2,
-                                              overflow: TextOverflow.ellipsis,
-                                              style: const TextStyle(
-                                                fontSize: 12.0,
-                                                color: Colors.black,
-                                              ),
+                                          ),
+                                          // const Padding(padding: EdgeInsets.only(bottom: 2.0)),
+                                          Text(snapshot.data![index].subtitle,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: const TextStyle(
+                                              fontSize: 12.0,
+                                              color: Colors.black,
                                             ),
-                                          ],
-                                        ),
+                                          ),
+                                          Wrap(
+                                            crossAxisAlignment: WrapCrossAlignment.start,
+                                            children: snapshot.data![index].tags.map((tag) => Padding(
+                                              padding: const EdgeInsets.only(right: 10),
+                                              child: Chip(
+                                                visualDensity: const VisualDensity(horizontal: -4, vertical: -4), // Chip size -4 -> 4
+                                                label: Text(tag.name),
+                                              ),
+                                            )).toList(),
+                                          ),
+                                        ],
                                       ),
-                                      Expanded(
-                                        child: Column(
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          mainAxisAlignment: MainAxisAlignment.end,
-                                          children: [
-                                            Text(snapshot.data![index].author,
-                                              style: const TextStyle(
-                                                fontSize: 12.0,
-                                                //color: Colors.black,
-                                              ),
+                                      const SizedBox(height: 5),
+                                      Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        mainAxisAlignment: MainAxisAlignment.end,
+                                        children: [
+                                          Text(snapshot.data![index].author,
+                                            style: const TextStyle(
+                                              fontSize: 12.0,
+                                              //color: Colors.black,
                                             ),
-                                            Text(formattedDate,
-                                              style: const TextStyle(
-                                                fontSize: 12.0,
-                                                //color: Colors.grey,
-                                              ),
+                                          ),
+                                          Text(formattedDate,
+                                            style: const TextStyle(
+                                              fontSize: 12.0,
+                                              //color: Colors.grey,
                                             ),
-                                          ],
-                                        ),
+                                          ),
+                                        ],
                                       ),
-                                      // Tags
-                                      // Padding(
-                                      //   padding: const EdgeInsets.symmetric(vertical: 5.0),
-                                      //   child: Wrap(
-                                      //     crossAxisAlignment: WrapCrossAlignment.start,
-                                      //     children: snapshot.data![index].tags.map((tag) => Padding(
-                                      //       padding: const EdgeInsets.only(right: 10),
-                                      //       child: Chip(
-                                      //         label: Text(tag.name),
-                                      //       ),
-                                      //     )).toList(),
-                                      //   ),
-                                      // ),
                                     ],
                                   ),
                                 ),
